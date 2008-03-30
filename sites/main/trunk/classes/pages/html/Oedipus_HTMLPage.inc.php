c0f17c878465587e5edace7b8eee870bae837185
robert.impey <robert.impey@ebd1daaf-a749-0410-bae2-fb04e7e5e931>
2008-03-30 18:05:42 +0000

    nav div.

--- sites/main/trunk/classes/pages/html/Oedipus_HTMLPage.inc.php
+++ sites/main/trunk/classes/pages/html/Oedipus_HTMLPage.inc.php
@@ -9,6 +9,11 @@ abstract class
 	Oedipus_HTMLPage
 extends
 	PublicHTML_HTMLPage
-{	
+{
+	public function
+		render_body_div_navigation()
+	{
+		Navigation_1DULRenderer::render_ul('Left Nav');
+	}
 }
 ?>
