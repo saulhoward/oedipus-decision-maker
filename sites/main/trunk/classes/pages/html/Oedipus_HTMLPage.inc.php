2f13ca5a1d834e62995d1f71cacd100cf69aa2b4
robert.impey <robert.impey@ebd1daaf-a749-0410-bae2-fb04e7e5e931>
2008-04-03 14:39:35 +0000

    Allowed access to the styles and images in the project-specific public-html page.

--- sites/main/trunk/classes/pages/html/Oedipus_HTMLPage.inc.php
+++ sites/main/trunk/classes/pages/html/Oedipus_HTMLPage.inc.php
@@ -6,23 +6,22 @@
  */
 
 abstract class
-Oedipus_HTMLPage
+	Oedipus_HTMLPage
 extends
-PublicHTML_HTMLPage
+	PublicHTML_HTMLPage
 {
 	public function
 		render_body_div_navigation()
 	{
 		Navigation_1DULRenderer::render_ul('Left Nav');
 	}
-
+	
 	public function
 		render_head_link_styles()
 	{
 
 	}
-
-
+	
 	public function 
 		render_head_link_stylesheet() 
 	{ 
@@ -33,22 +32,16 @@ PublicHTML_HTMLPage
 		HTMLTags_LinkRenderer 
 			::render_style_sheet_link( 
 				'http://yui.yahooapis.com/2.3.1/build/base/base-min.css' 
-			); 
+			);
 		HTMLTags_LinkRenderer 
 			::render_style_sheet_link( 
-				'/styles/styles.css' 
+				'/styles/style.css' 
 			); 
-	} 
-
-	/** 
-	 * This is a bit ugly but I'm not sure how else to do this. 
-	 * 
-	 * The default action is to print nothing but this can be extended. 
-	 */ 
+	}
+	
 	public function 
 		render_head_script_javascript() 
 	{ 
 	}
-
 }
 ?>
