97fdd6d846a9f50a4de64a980f84ad84e1f9eb76
saulhoward <saulhoward@ebd1daaf-a749-0410-bae2-fb04e7e5e931>
2008-05-17 00:56:14 +0000

    pngs of tables working, albeit ugly

--- sites/main/trunk/classes/pages/png/Oedipus_TablePNGImage.inc.php
+++ sites/main/trunk/classes/pages/png/Oedipus_TablePNGImage.inc.php
@@ -5,6 +5,8 @@
  * @copyright 2008-05-15, SANH
  *
  * Creates a PNG of an Oedipus Table
+ * Uses the GD library,
+ * and PostScript fonts
  *
  */
 
@@ -22,11 +24,20 @@ Oedipus_GDPNGImage
 	# GD Image attributes
 	private $font;
 	private $padding;
+	private $label_width;
+	private $label_height;
+	private $label_indent_width;
+	private $table_width;
+	private $table_height;
+	private $column_width;
+	private $table_name_label_height;
+	private $font_size;
 	private $font_color;
 	private $font_shadow_color;
 	private $padding_color;
 	private $background_color;
 	private $table_background_color;
+	private $stated_intention_background_color;
 
 	public function
 		render()
@@ -35,6 +46,14 @@ Oedipus_GDPNGImage
 		{
 			$this->table =
 				Oedipus_TableCreationHelper::get_oedipus_table_by_id($_GET['table_id']);
+
+			$this->set_padding(10);
+			$this->set_label_width_and_height(200, 50);
+			$this->set_label_indent_width(10);
+			$this->set_table_name_label_height(50);
+			$this->set_column_width(50);
+			$this->set_font('caslon.pfb');
+			$this->set_font_size(16);
 			$this->set_oedipus_table_image();
 		}
 
@@ -48,41 +67,264 @@ Oedipus_GDPNGImage
 	private function
 		set_oedipus_table_image()
 	{
-		// Find the Size of table and image
-		// width = label + (1 X no_of_actors) + 1 for si
-		// height = 1 for header + (1 x no_of_actors)
-		$label_width = 200;
-		$label_height = 50;
-		$no_of_actors = $this->get_no_of_actors();
+		$this->set_table_height_and_width();
 
-		$table_width = $label_width + (100 * $no_of_actors) + 100;
-		$table_height = ($label_height * $no_of_actors) + 100;
-		$table_name_label_height = 50;
-
-		$this->set_padding();
-		$image_width = $table_width + ($this->padding * 2);
-		$image_height = $table_name_label_height + $table_height + ($this->padding * 2);
+		$image_width = $this->table_width + ($this->padding * 2);
+		$image_height = $this->table_name_label_height + $this->table_height + ($this->padding * 2);
 
 		// Set the image and the colors
 		$this->image = imagecreatetruecolor($image_width, $image_height);
 		$this->set_colors();
-		$this->set_font();
 
 		// fill image with background color
 		imagefill($this->image, 0, 0, $this->background_color);
 
-		//  imagefilledellipse($this->image, 400, 300, 400, 300, $red);
-		//  $this->gradient_region(10, 10, 200, 200, $this->font_color, $this->padding_color);
-
 		// Set the table name label
-		$this->set_table_name_label();
+		$this->draw_table_name_label();
 
 		// Draw a box for the table
 		imagefilledrectangle($this->image,
-			$this->padding, $this->padding + $table_name_label_height,        
-			$table_width + $this->padding, $table_height + $this->padding + $table_name_label_height,
+			$this->padding, $this->padding + $this->table_name_label_height,        
+			$this->table_width + $this->padding,
+		       	$this->table_height + $this->padding + $this->table_name_label_height,
 			$this->table_background_color
 		);
+
+		// Draw the Column headings and backgrounds
+		$this->draw_column_headings_and_backgrounds();
+
+		// Draw the option labels and tiles
+		$this->draw_option_labels_and_tiles();
+
+	}
+
+	private function
+		draw_option_labels_and_tiles()
+	{
+		// Foreach option, 
+		// write the actors name if this is his/her first option
+		// write the option label,
+		// and put in a tile for all actors and one for the s.i.
+
+		$x = 0;
+		$y = 0;
+		foreach ($this->table->get_actors() as $actor)
+		{
+			if ($actor->has_options())
+			{
+				$first = TRUE;
+				foreach ($actor->get_options() as $option)
+				{
+					if ($first)
+					{
+						//put the x,y in the first column
+						$x = $this->padding;
+						$y = $this->padding + $this->table_name_label_height + $this->label_height;
+
+						//print the actors name
+						$this->draw_option_label($actor->get_name(), $x, $y);
+
+						// move onto the next row
+						$y += $this->label_height;
+
+						$first = FALSE;
+					}
+
+					//indent x
+					$x += $this->label_indent_width;
+					//print the options label
+					$this->draw_option_label($option->get_name(), $x, $y);
+
+					//move x to the first actor
+					$x = $this->padding + $this->label_width;
+
+					foreach ($this->table->get_actors() as $position_actor)
+					{
+						$position = $option->get_position($position_actor->get_id());
+
+						//draw the tile
+						$this->draw_position_tile($position, $position_actor, $x, $y);
+
+						// advance x
+						$x += $this->column_width;
+					}
+
+					$stated_intention = $option->get_stated_intention();
+					//draw the tile
+					$this->draw_stated_intention_tile($stated_intention, $actor, $x, $y);
+
+					// move onto the next row
+					$y += $this->label_height;
+					// reset x to the beginning of the line
+					$x = $this->padding;
+				}
+			}
+		}
+	}
+
+	private function
+		draw_option_label($label_text, $x, $y)
+	{
+		imagepstext(
+			$this->image,
+			$label_text, 
+			$this->font, 
+			$this->font_size, 
+			$this->font_color,
+			$this->table_background_color,
+			$x, $y
+		);
+	}
+
+	private function
+		draw_position_tile($position, $position_actor, $x, $y)
+	{
+		$position_position= $position->get_tile();
+		switch ($position_position)
+		{
+			case '1':
+				$position_filename_part = '_filled';
+				break;
+			case '0':
+			default:
+				$position_filename_part = '_empty';
+		}
+
+		$position_doubt= $position->get_doubt();
+		switch ($position_doubt)
+		{
+			case '?':
+				$doubt_filename_part = '_question';
+				break;
+			case 'x':
+				$doubt_filename_part = '_x';
+				break;
+			case '':
+			default:
+				$doubt_filename_part = '';
+		}
+
+//                print_r(
+//                        "/project-specific/public-html/images/position-tiles/40px-png/squares/"
+//                        . $position_actor->get_color()
+//                        . $position_filename_part
+//                        . $doubt_filename_part
+//                );exit;
+
+		$tile = $this->load_png(
+			PROJECT_ROOT 
+			. "/project-specific/public-html/images/position-tiles/40px-png/squares/"
+			. $position_actor->get_color()
+			. $position_filename_part
+			. $doubt_filename_part
+			. '.png'
+		);
+
+		imagecopy($this->image, $tile, $x, $y, 0, 0, 40, 40);
+	}
+
+	private function
+		draw_stated_intention_tile(Oedipus_StatedIntention $stated_intention, Oedipus_Actor $actor, $x, $y)
+	{
+		$stated_intention_position= $stated_intention->get_tile();
+		switch ($stated_intention_position)
+		{
+			case '1':
+				$position_filename_part = '_filled';
+				break;
+			case '0':
+			default:
+				$position_filename_part = '_empty';
+		}
+
+		$stated_intention_doubt= $stated_intention->get_doubt();
+		switch ($stated_intention_doubt)
+		{
+			case '?':
+				$doubt_filename_part = '_question';
+				break;
+			case 'x':
+				$doubt_filename_part = '_x';
+				break;
+			case '':
+			default:
+				$doubt_filename_part = '';
+		}
+
+//                print_r(
+//                        "/project-specific/public-html/images/position-tiles/40px-png/squares/"
+//                        . $position_actor->get_color()
+//                        . $position_filename_part
+//                        . $doubt_filename_part
+//                );exit;
+
+		$tile = $this->load_png(
+			PROJECT_ROOT 
+			. "/project-specific/public-html/images/position-tiles/40px-png/diamonds/"
+			. $actor->get_color()
+			. $position_filename_part
+			. $doubt_filename_part
+			. '.png'
+		);
+
+		imagecopy($this->image, $tile, $x, $y, 0, 0, 50, 50);
+	}
+
+	private function
+		set_table_height_and_width()
+	{
+		// Find the Size of table and image
+		// width = label + (1 X no_of_actors) + 1 for si
+		// height = 1 for header + (1 x no_of_actors)
+		$no_of_actors = $this->get_no_of_actors();
+
+		$this->table_width = $this->label_width + ($this->column_width * $no_of_actors) + 100;
+		$this->table_height = ($this->label_height * $no_of_actors) + 100;
+	}
+
+	private function
+		draw_column_headings_and_backgrounds()
+	{
+		$x = 0;
+		$y = 0;
+		$first = TRUE;
+
+		// go through the actors
+		foreach ($this->table->get_actors() as $actor)
+		{
+			if ($first)
+			{
+				//put the x,y in the first column
+				$x = $this->padding + $this->label_width;
+				$y = $this->padding + $this->table_name_label_height;
+			}
+
+			// Write the heading label 
+			$this->draw_heading_label_for_actor($actor, $x, $y);
+
+			// Draw a box for the column background
+			imagefilledrectangle(
+				$this->image,
+				$x, $y,        
+				$x + $this->column_width, $y + $this->table_height,
+				$this->get_actors_background_color($actor)
+			);
+
+			// advance x to the next column
+			$x += $this->column_width;
+			$first = FALSE;
+		}
+
+		// Write the s.i. label 
+		$this->draw_heading_label_for_stated_intention('S.I.', $x, $y);
+
+		// draw the s.i. column background 
+		imagefilledrectangle(
+				$this->image,
+				$x, $y,        
+				$x + $this->column_width, $y + $this->table_height,
+				$this->stated_intention_background_color
+			);
 	}
 
 	private function
@@ -102,39 +344,122 @@ Oedipus_GDPNGImage
 		$this->padding_color =  $lightgrey;
 		$this->background_color =  $yellow;
 		$this->table_background_color =  $lightgrey;
+		$this->stated_intention_background_color =  $black;
 	}
 
 	private function
-		set_padding()
+		get_actors_background_color(Oedipus_Actor $actor)
 	{
-		$this->padding = 10;
+		// define some colors
+		$red = imagecolorallocate($this->image, 255, 0, 0);
+		$blue = imagecolorallocate($this->image, 0, 0, 255);
+		$white = imagecolorallocate($this->image,255,255,255);
+		$black = imagecolorallocate($this->image,0,0,0);
+		$lightgrey = imagecolorallocate($this->image, 200, 200, 200);
+		$grey = imagecolorallocate($this->image,100,100,100);
+		$yellow = imagecolorallocate($this->image, 0xFF, 0xFF, 0x00);
+
+		$color = $actor->get_color();
+		switch ($color)
+		{
+			case 'red':
+				return $red;
+			case 'blue':
+				return $blue;
+			default:
+				return $red;
+		}
 	}
 
 	private function
-		set_font()
+		set_label_width_and_height($width = 200, $height = 50)
 	{
-		$this->font = PROJECT_ROOT . '/project-specific/public-html/fonts/Vera.ttf';
+		$this->label_width = $width;
+		$this->label_height = $height;
 	}
 
 	private function
-		set_table_name_label()
+		set_label_indent_width($amount = 10)
+	{
+		$this->label_indent_width = $amount;
+	}
+
+	private function
+		set_font_size($pixels = 12)
+	{
+		$this->font_size = $pixels;
+	}
+
+	private function
+		set_table_name_label_height($amount = 50)
+	{
+		$this->table_name_label_height = $amount;
+	}
+
+	private function
+		set_column_width($amount = 50)
+	{
+		$this->column_width = $amount;
+	}
+
+	private function
+		set_padding($amount = 10)
+	{
+		$this->padding = $amount;
+	}
+
+	private function
+		set_font($font_name = 'caslon.pfb')
+	{
+		$this->font = imagepsloadfont(PROJECT_ROOT . '/project-specific/public-html/fonts/' . $font_name);
+	}
+
+	private function
+		draw_heading_label_for_actor($actor, $x, $y)
+	{
+		$actor_background_color = $this->get_actors_background_color($actor);
+
+		imagepstext(
+			$this->image,
+			$actor->get_short_name(), 
+			$this->font, 
+			$this->font_size, 
+			$this->font_color,
+			$actor_background_color,
+			$x, $y
+		);
+	}
+
+	private function
+		draw_heading_label_for_stated_intention($label_text, $x, $y)
+	{
+		imagepstext(
+			$this->image,
+			$label_text, 
+			$this->font, 
+			$this->font_size, 
+			$this->font_color,
+			$this->stated_intention_background_color,
+			$x, $y
+		);
+	}
+
+	private function
+		draw_table_name_label()
 	{
 		// The text to draw
 		$text = $this->table->get_name();
-		// Replace path by your own font path
 
-		$table_name_label_height = 30;
 		// Add the text
-//                imagepstext(
-		imagettftext(
+		imagepstext (
 			$this->image,
-			20, 0,
-			$this->padding, $this->padding + $table_name_label_height,
-			$this->font_color,
+			$text,
 			$this->font, 
-			$text
+			$this->font_size, 
+			$this->font_color,
+			$this->background_color,
+			$this->padding, $this->table_name_label_height - $this->padding
 		);
-
 	}
 
 	private function
