394bd9c084ab3d6f84aaf8deaafaa9ef06751432
saulhoward <saulhoward@ebd1daaf-a749-0410-bae2-fb04e7e5e931>
2008-11-23 00:43:08 +0000

    js edit form hiding & default content

--- sites/main/trunk/public-html/scripts/Oedipus_DramaPage.js
+++ sites/main/trunk/public-html/scripts/Oedipus_DramaPage.js
@@ -58,5 +58,109 @@ $(document).ready(function() {
 			splitTitle: '|',
 			arrows: true
 		});
+
+
+		/*
+		 *HIde the Forms and replace with divs
+		 */
+		if ($('.scene-form').length) {
+			replace_form_with_content_from_element_value(
+				'#name-form',
+				'.scene-form',
+				'#scene_name',
+				'h3'
+				);
+		}
+		if ($('.frame-form').length) {
+			replace_form_with_content_from_element_value(
+				'#name-form',
+				'.frame-form',
+				'#frame_name',
+				'h3'
+				);
+		}
+	
+		if ($('.edit-scene-note').length) {
+				replace_form_with_content_from_div(
+					'#note-form', // String like '.notes'
+					'.edit-scene-note',
+					'.note-preview'
+					);
+				}
+	
+		if ($('.edit-frame-note').length) {
+				replace_form_with_content_from_div(
+					'#note-form', // String like '.notes'
+					'.edit-frame-note',
+					'.note-preview'
+					);
+				}
 });
 
+function 
+replace_form_with_content_from_element_value(
+		$parent_div_css, // String like '.notes'
+		$form_css,
+		$content_css,
+		$replacement_tag // tag like h3
+		)
+{
+	$($parent_div_css).each(function(){
+
+			var $parent_div = $(this);
+			var $form = $parent_div.find($form_css).hide();
+			var $content = $parent_div.find($content_css).val();
+
+			$form.before('<div class="replacement"><' + $replacement_tag  + '>' + $content + '</' + $replacement_tag + '></div>');
+			var $replacement = $parent_div.find('div.replacement');
+			$replacement.prepend('<p class="hover-edit-link">click to edit...</p>');
+			$parent_div.find('p.hover-edit-link').hide();
+			$replacement.hover(
+				function(){
+				$parent_div.find('p.hover-edit-link').show();
+				},
+				function(){
+				$parent_div.find('p.hover-edit-link').hide();
+				}
+				);
+			$replacement.click(function(){
+				$replacement.hide();
+				$parent_div.find('p.hover-edit-link').hide();
+				$form.fadeIn();
+				});
+			});
+}
+
+function 
+replace_form_with_content_from_div(
+		$parent_div_css, // String like '.notes'
+		$form_css,
+		$content_div_css
+		)
+{
+	$($parent_div_css).each(function(){
+
+			var $parent_div = $(this);
+			var $form = $parent_div.find($form_css).hide();
+			var $content = $parent_div.find($content_div_css).html();
+			$parent_div.find($content_div_css).hide();
+
+			$form.before('<div class="replacement">' + $content + '</div>');
+			var $replacement = $parent_div.find('div.replacement');
+			$replacement.prepend('<p class="hover-edit-link">click to edit...</p>');
+			$parent_div.find('p.hover-edit-link').hide();
+			$replacement.hover(
+				function(){
+				$parent_div.find('p.hover-edit-link').show();
+				},
+				function(){
+				$parent_div.find('p.hover-edit-link').hide();
+				}
+				);
+			$replacement.click(function(){
+				$replacement.hide();
+				$parent_div.find('p.hover-edit-link').hide();
+				$form.fadeIn();
+				});
+			});
+}
