991897ec26e3cb87636157225341b6ab15abcfbe
robert.impey <robert.impey@ebd1daaf-a749-0410-bae2-fb04e7e5e931>
2010-08-22 10:42:39 +0000

    An attempt at a query to replace the multiple select statements in Oedipus_DramaFactory::get_frame_by_id(...).

---
+++ sites/main/trunk/sql/useful-queries/select-frame.sql
@@ -0,0 +1,20 @@
+# This select is intended to replace the multiple select statements in
+# Oedipus_DramaFactory::get_frame_by_id(...)
+
+SELECT
+    f.id frame_id
+    , f.name frame_name
+    , c.name character_name
+    , o.name option_name
+    , si.position stated_intention_position
+    , si.doubt stated_intention_doubt
+    , p.position position_position
+    , p.doubt position_doubt
+FROM 
+    oedipus_frames f
+        LEFT JOIN oedipus_characters c ON f.id = c.frame_id
+        LEFT JOIN oedipus_options O ON c.id = o.character_id
+        LEFT JOIN oedipus_stated_intentions si ON si.id = o.stated_intention_id
+        LEFT JOIN oedipus_positions p ON o.id = p.option_id AND c.id = p.character_id
+WHERE
+    f.id = 12;
