5d6274545cf6b0095225cb657f6ee2f23500d713
robert.impey <robert.impey@ebd1daaf-a749-0410-bae2-fb04e7e5e931>
2011-05-07 17:06:32 +0000

    SQL statements to clear the database.

---
+++ sites/main/trunk/sql/useful-queries/nuke-oedipus.sql
@@ -0,0 +1,19 @@
+-- To clear the databases before tests are run.
+
+TRUNCATE TABLE oedipus_users_allowed_to_view_drama_links;
+TRUNCATE TABLE oedipus_frame_to_note_links;
+TRUNCATE TABLE oedipus_scene_to_note_links;
+TRUNCATE TABLE oedipus_frame_trees;
+
+TRUNCATE TABLE oedipus_notes;
+
+TRUNCATE TABLE oedipus_positions;
+TRUNCATE TABLE oedipus_options;
+TRUNCATE TABLE oedipus_stated_intentions;
+TRUNCATE TABLE oedipus_characters;
+TRUNCATE TABLE oedipus_frames;
+TRUNCATE TABLE oedipus_scenes;
+TRUNCATE TABLE oedipus_acts;
+TRUNCATE TABLE oedipus_dramas;
+
+TRUNCATE TABLE oedipus_users;
